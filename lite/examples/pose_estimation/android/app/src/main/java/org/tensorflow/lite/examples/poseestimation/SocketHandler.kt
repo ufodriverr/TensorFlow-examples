5feaa46da66fa2a3a18ec2f053a63bc35dd15aa5
Vladimir Ilianov <ufodriverr@mail.ru>
2022-06-23 13:35:48 +0200

    ы

--- lite/examples/pose_estimation/android/app/src/main/java/org/tensorflow/lite/examples/poseestimation/SocketHandler.kt
+++ lite/examples/pose_estimation/android/app/src/main/java/org/tensorflow/lite/examples/poseestimation/SocketHandler.kt
@@ -3,6 +3,8 @@ import android.util.Log
 import io.socket.client.IO
 import io.socket.client.Socket
 import io.socket.client.SocketOptionBuilder
+import io.socket.engineio.client.transports.WebSocket
+import java.net.URI
 import java.net.URISyntaxException
 
 object SocketHandler {
@@ -25,7 +27,9 @@ object SocketHandler {
                     "pin" to ""
                 )
             )
-            mSocket = IO.socket("http://192.168.77.134:3000", builder.build())
+            builder.setReconnection(false);
+            builder.setTransports(arrayOf(WebSocket.NAME));
+            mSocket = IO.socket(URI.create("http://localhost:3000"), builder.build())
         } catch (e: URISyntaxException) {
             Log.d("Unity", e.toString())
         }
--- lite/examples/pose_estimation/android/app/src/main/java/org/tensorflow/lite/examples/poseestimation/camera/CameraSource.kt
+++ lite/examples/pose_estimation/android/app/src/main/java/org/tensorflow/lite/examples/poseestimation/camera/CameraSource.kt
@@ -50,8 +50,8 @@ class CameraSource(
 ) {
 
     companion object {
-        private const val PREVIEW_WIDTH = 1280
-        private const val PREVIEW_HEIGHT = 720
+        private const val PREVIEW_WIDTH = 640
+        private const val PREVIEW_HEIGHT = 480
 
         /** Threshold for confidence score. */
         private const val MIN_CONFIDENCE = .2f
